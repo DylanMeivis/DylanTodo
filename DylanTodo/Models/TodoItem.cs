dd9bc92279d3ddf96673c818dbdbe39cc425a0f3
Dylan Meivis <dylandiamonds@live.nl>
2019-09-10 16:51:34 +0200

    🐳 Added models and Layout

---
+++ DylanTodo/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DylanTodo.Models
+{
+    public class TodoItem
+    {
+        private Guid Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public TodoItem(string desc)
+        {
+            Description = desc;
+            Id = Guid.NewGuid();
+            IsCompleted = false;
+        }
+    }
+}
